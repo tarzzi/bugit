844a62c6724892302a38a2a9bf4dfbf521a3d519
Tarmo <n4378@student.jamk.fi>
2021-06-11 23:33:03 +0300

    initial Nav component built

---
+++ bugit/components/Nav.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import navStyles from '../styles/Nav.module.scss'
+
+const Nav = () => {
+    return(
+        <nav className={navStyles.nav}>
+            <ul>
+                <li className={navStyles.logo}>
+                    Bugit
+                </li>
+                <li>
+                    <Link href='/'>Home</Link>
+                </li>
+                <li>
+                    <Link href='/projects'>Projects</Link>
+                </li>
+                <li>
+                    <Link href='/settings'>Settings</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+
+}
+
+export default Nav
