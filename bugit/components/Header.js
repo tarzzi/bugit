fa6597611ddf46daba2d523b19da9f16f44d3b6b
Tarmo <n4378@student.jamk.fi>
2021-06-11 23:32:07 +0300

    test header set up

---
+++ bugit/components/Header.js
@@ -0,0 +1,12 @@
+import headerStyles from '../styles/Header.module.scss'
+
+const Header = () => {
+    return(
+        <div>
+            <h1 className={headerStyles.title}>
+                Header
+            </h1>
+        </div>
+    )
+}
+export default Header
