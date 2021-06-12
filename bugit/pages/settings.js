fee62099acc9c7f5287bfdfd4161ed483801d3f1
Tarmo <n4378@student.jamk.fi>
2021-06-12 20:51:49 +0300

    Create settings page and styles

--- bugit/pages/settings.js
+++ bugit/pages/settings.js
@@ -1,7 +1,24 @@
+import Link from 'next/link'
+import settingsStyles from '../styles/Settings.module.scss'
+
 const settings = () => {
     return(
         <div>
             <h1>Settings</h1>
+            <ul className={settingsStyles.list}>
+                <li>
+                    <Link href='/resetpwd'>
+                        🔐 Change password
+                    </Link></li>
+                <li>
+                    <Link href='/more-settings'>
+                        ⚙️More settings
+                    </Link></li>
+                <li>
+                    <Link href='/user'>
+                        💡 User settings
+                    </Link></li>
+            </ul>
         </div>
     )
 }
