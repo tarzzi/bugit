3a40b90ec1d38bd3d13e24001df860cff5867b80
Tarmo <n4378@student.jamk.fi>
2021-06-12 22:11:29 +0300

    edit to use Meta component

--- bugit/pages/settings.js
+++ bugit/pages/settings.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import settingsStyles from '../styles/Settings.module.scss'
+import Meta from '../components/Meta'
 
 const settings = () => {
     return(
         <div>
+            <Meta title='Settings' />
             <h1>Settings</h1>
             <ul className={settingsStyles.list}>
                 <li>
