2fa23a7a911b8e5255accc8804dfa4cd2e2e6ddf
Tarmo <n4378@student.jamk.fi>
2021-06-12 22:08:40 +0300

    edit Layout to use Meta component

--- bugit/components/Layout.js
+++ bugit/components/Layout.js
@@ -1,9 +1,11 @@
 import Nav from './Nav'
+import Meta from './Meta'
 import styles from '../styles/Layout.module.scss'
 
 const Layout = ({children}) => {
     return(
         <>
+          <Meta />
             <Nav />
             <div className={styles.container}>
                 <main className={styles.main}>
